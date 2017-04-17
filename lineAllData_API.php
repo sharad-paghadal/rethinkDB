2c2c2b1739dfc867af4f30d8eb7bf02ce9752150
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-04-17 14:25:11 +0530

    change in line all data

--- lineAllData_API.php
+++ lineAllData_API.php
@@ -6,15 +6,12 @@
 
 	$symbolCode = $_REQUEST["code"];
 
-		$result = r\db("protrade")->table("rawvalue")->orderBy(array("index" => "id"))->filter(array("code" => $symbolCode))->pluck(array("price"))->run($conn);
+		$result = r\db("protrade")->table("rawvalue")->orderBy(array("index" => "id"))->filter(array("code" => $symbolCode))->pluck(array("price","time_stamp"))->run($conn);
 		$count = r\db("protrade")->table("rawvalue")->filter(array("code" => $symbolCode))->count()->run($conn);
 
 	    $response = array();
 	    $response['data'] = array();
-	    $response['status'] = "";
 	    $response['count'] = $count;
-
-	    $response['status'] = "SUCCESS";
 
 	    foreach ($result as $doc) {
 	    	array_push($response['data'], $doc);
