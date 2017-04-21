da59bfe209e2b4e41c061bc6a19e32c33ab4d56b
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-04-21 19:38:41 +0530

    AllLineChart API Proper working

--- lineAllData_API.php
+++ lineAllData_API.php
@@ -6,15 +6,18 @@
 
 	$symbolCode = $_REQUEST["code"];
 
-		$result = r\db("protrade")->table("rawvalue")->orderBy(array("index" => "id"))->filter(array("code" => $symbolCode))->pluck(array("price","time_stamp"))->run($conn);
-		$count = r\db("protrade")->table("rawvalue")->filter(array("code" => $symbolCode))->count()->run($conn);
+		$result = r\db("protrade")->table("rawvalue")->orderBy(array("index" => "id"))->filter(array("code" => $symbolCode))->pluck(array("current_price","time_stamp"))->run($conn);
 
 	    $response = array();
-	    $response['data'] = array();
-	    $response['count'] = $count;
 
 	    foreach ($result as $doc) {
-	    	array_push($response['data'], $doc);
+	    	$tempData = array();
+	    	$time = strtotime($doc['time_stamp']);
+	    	$price = (int) $doc['current_price'];
+	    	array_push($tempData, $time);
+	    	array_push($tempData, $price);
+	    	
+	    	array_push($response, $tempData);
     	}
 
 	    echo json_encode($response);
