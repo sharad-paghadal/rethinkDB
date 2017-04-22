5077778bd236f55c465dc6be3babd5cb8228c0f1
Sharad Paghadal <sharad.appdeveloper@gmail.com>
2017-04-22 11:40:55 +0530

    Candle stick all data api proper working

---
+++ candleStickAllData_API.php
@@ -0,0 +1,36 @@
+<?php
+
+//URL : url?code=value&cycle=value
+
+    require_once("rdb/rdb.php");
+    require_once("constants.php");
+    $conn = r\connect(DB_HOST);
+
+    $symbolCode = $_REQUEST['code'];
+    $tableName = "cycle_".$_REQUEST['cycle'];
+
+    $result = r\db("trade_cycle")->table($tableName)->orderBy(array("index" => "id"))->filter(array("code" => $symbolCode))->run($conn);
+
+    $response = array();
+
+    foreach ($result as $doc) {
+    	$tempData = array();
+    	$time = strtotime($doc['time_stamp']);
+    	$open = (float) $doc['open'];
+    	$high = (float) $doc['high'];
+    	$low = (float) $doc['low'];
+    	$close = (float) $doc['close'];
+
+
+    	array_push($tempData, $time);
+    	array_push($tempData, $open);
+    	array_push($tempData, $high);
+    	array_push($tempData, $low);
+    	array_push($tempData, $close);
+
+    	array_push($response, $tempData);
+	}
+    echo json_encode($response);
+
+    $conn->close();
+?>
